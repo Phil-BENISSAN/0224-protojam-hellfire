af5d0dd1d6bd7d0cfdc5503eedb99fd2522d7ece
arobert004 <anthony.robert.pro1@gmail.com>
2024-05-16 15:54:30 +0200

    add testpollution

--- data-fastapi/app/app_fastapi.py
+++ data-fastapi/app/app_fastapi.py
@@ -1,14 +1,40 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
-@app.get('/')
-def fn_fast_api():
 
-# -------  INSERER VOTRE CODE ICI -----------------
+# FAST CLEANING
+df = pd.read_csv(r'C:\WCS\Python\HELLFIRE\0224-protojam-hellfire\data-fastapi\app\testpolution (2).csv', usecols=[
+                                                    'Country',
+                                                    'Geography',
+                                                    'Value'
+                                                    ])
+
+
+
+
+app = FastAPI(doc_urls = "/documentation")
+
+
+@app.get("/")
+def root():
     
-    return {'Key': 'Value'}
+    return {"message": "Hello World"}
+
+
+@app.get("/all")
+def city():
+
+    return df[['Geography','Value']].set_index('Geography').to_dict()['Value']
+
+
+@app.get("/all/{country}")
+def find_country(country):
+
+    return df[df['Country'] == country].iloc[:,-2:].set_index('Geography').to_dict()['Value']
 
 
 # ---------------- FIN DE TON CODE ----------------
