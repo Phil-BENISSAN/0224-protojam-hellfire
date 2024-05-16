6ba6a675272513410b3df887135eff14cc6e0855
Franck <contact@franckwebpro.com>
2024-05-16 11:48:45 +0200

    basic css

---
+++ client/src/components/Navbar/NavLink.jsx
@@ -0,0 +1,7 @@
+export function Navlink() {
+  return (
+    <main>
+      <p>navlink </p>
+    </main>
+  );
+}
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Navlink } from "./NavLink";
+
+export function Navbar() {
+  return (
+    <main>
+      <p>Navbar</p>
+      <Navlink />
+    </main>
+  );
+}
